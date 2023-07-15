049c46c49660b8eca3c4cc7579d94c3de242fcc5
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-07-15 08:26:04 -0400

    post test on index, github actions added

--- hello/tests.py
+++ hello/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
 
 # Create your tests here.
+class TestHello(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_index_get(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello World")
+    
+    def test_index_post(self):
+        data =  {"note": "Lorem Ipsum"}
+        response = self.client.post("/", data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, data["note"])
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.views import View
 
+data = []
+
 
 class IndexView(View):
     def get(self, request):
         return HttpResponse("Hello World!")
+    
+    def post(self, request):
+        note = request.POST.get("note")
+        data.append(note)
+        return HttpResponse(data)
